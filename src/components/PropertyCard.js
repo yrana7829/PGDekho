8cda493e6661dce607e6b6af09d46b909c8b9622
RUBY RANA <75352622+ruby78297@users.noreply.github.com>
2023-06-17 14:29:36 +0530

    Update PropertyCard.js

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,54 +1,88 @@
-import React from "react";
-import { Card, Row, Col, Button, CardImg, Nav } from "react-bootstrap";
-import properties from "../product";
+import React from 'react';
+import { Card, Row, Col, Button, CardImg, Nav } from 'react-bootstrap';
+import { FaRuler, FaBed, FaBath, FaCar } from 'react-icons/fa';
+import properties from '../product';
 
 const PropertyCard = () => {
   return (
     <Row>
       {properties.map((property) => (
-        <Col key={property.id} className="mb-4">
-          <Card
-            className="horizontal-card"
-            style={{ width: "400px", height: "354px" }}
-          >
+        <Col key={property.id} className='mb-4'>
+          <Card className='property-card'>
             <CardImg
               src={property.areaImage}
-              variant="top"
-              className="property-image"
+              variant='top'
+              className='property-image'
             />
-            <Card.Body className="d-flex flex-column justify-content-between">
+            <Card.Body className='d-flex flex-column justify-content-between'>
               <div>
-                <Card.Title>{property.name}</Card.Title>
-                <Card.Text>{property.location}</Card.Text>
+                <Card.Title className='Room-Title'>{property.name}</Card.Title>
+                <Card.Text className='Room-location'>
+                  {property.location}
+                </Card.Text>
               </div>
               <div>
-                <Row>
-                  <Col>
-                    <p>Single Room: ${property.roomPrices.singleRoom}</p>
+                <Row className='Room-price-div'>
+                  <Col className='Room-price-cols'>
+                    <div className='Room-occupancy'>
+                      <span>Single</span>
+                      <span className='Room-occupancy-price'>
+                        ${property.roomPrices.singleRoom}
+                      </span>
+                    </div>
                   </Col>
-                  <Col>
-                    <p>Double Room: ${property.roomPrices.doubleRoom}</p>
+                  <Col className='Room-price-cols'>
+                    <div className='Room-occupancy'>
+                      <span>Double</span>
+                      <span className='Room-occupancy-price'>
+                        ${property.roomPrices.doubleRoom}
+                      </span>
+                    </div>
                   </Col>
-                  <Col>
-                    <p>Three Room: ${property.roomPrices.threeRoom}</p>
+                  <Col className='Room-price-cols'>
+                    <div className='Room-occupancy'>
+                      <span>Three</span>
+                      <span className='Room-occupancy-price'>
+                        ${property.roomPrices.threeRoom}
+                      </span>
+                    </div>
                   </Col>
                 </Row>
-                <Row>
-                  <Col>
-                    <p>Area: {property.area} sqft</p>
+                <Row className='Area-details'>
+                  <Col xs={8}>
+                    <Row className='justify-content-center align-items-center'>
+                      <Col className='text-center mx-2'>
+                        <FaRuler className='Room-area-icon' />
+                        <p className='Room-area-details'>
+                          {property.area} sqft
+                        </p>
+                      </Col>
+                      <Col className='text-center mx-2'>
+                        <FaBed className='Room-area-icon' />
+                        <p className='Room-area-details'>
+                          {property.totalBeds}
+                        </p>
+                      </Col>
+                      <Col className='text-center mx-2'>
+                        <FaBath className='Room-area-icon' />
+                        <p className='Room-area-details'>{property.bath}</p>
+                      </Col>
+                      <Col className='text-center mx-2'>
+                        <FaCar className='Room-area-icon' />
+
+                        <p className='Room-area-details'>
+                          {property.garage ? 'Yes' : 'No'}
+                        </p>
+                      </Col>
+                    </Row>
                   </Col>
-                  <Col>
-                    <p>Total Beds: {property.totalBeds}</p>
-                  </Col>
-                  <Col>
-                    <p>Bathrooms: {property.bath}</p>
-                  </Col>
-                  <Col>
-                    <p>Garage: {property.garage ? "Yes" : "No"}</p>
+                  <Col xs={4}>
+                    <Button variant='success' className='w-100 Room-card-btn'>
+                      Book
+                    </Button>
                   </Col>
                 </Row>
               </div>
-              <Button variant="success">Book</Button>
             </Card.Body>
           </Card>
         </Col>
